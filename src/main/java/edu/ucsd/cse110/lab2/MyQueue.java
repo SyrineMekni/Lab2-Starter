ee78299c8c317ff4a968e60d057226694f1ecca2
Syrine Mekni <syr.mekni@gamil.com>
2026-01-16 18:57:31 -0800

    Fix MyQueue unit tests

--- src/main/java/edu/ucsd/cse110/lab2/MyQueue.java
+++ src/main/java/edu/ucsd/cse110/lab2/MyQueue.java
@@ -5,49 +5,50 @@ public class MyQueue {
     private int front;
     private int rear;
     private int size;
-    
+
     public MyQueue(int capacity) {
         elements = new int[capacity];
         front = 0;
         rear = -1;
         size = 0;
     }
-    
+
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
     public boolean isFull() {
-        return size == elements.length-1;
+        return size == elements.length; // fixed
     }
-    
+
     public int size() {
         return size;
     }
-    
-    public void enqueue(int element) {
-        if (isFull()) {
-            throw new IllegalStateException("Queue is full");
-        }
-        rear = (rear + 1) % elements.length;
-        elements[rear] = element;
-        size++;
+
+   public void enqueue(int element) {
+    if (isFull()) {
+        throw new IllegalStateException("Queue is full");
     }
-    
+    rear = (rear + 1) % elements.length;
+    elements[rear] = element;
+    size++;
+}
+
+
     public int dequeue() {
         if (isEmpty()) {
             throw new IllegalStateException("Queue is empty");
         }
         int element = elements[front];
         front = (front + 1) % elements.length;
-        size++;
+        size--; // fixed
         return element;
     }
-    
+
     public int peek() {
         if (isEmpty()) {
             throw new IllegalStateException("Queue is empty");
         }
-        return elements[rear];
+        return elements[front]; // fixed
     }
 }
